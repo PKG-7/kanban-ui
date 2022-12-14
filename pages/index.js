71ce5571487ea1c0e0824b2a854095a036b1040c
PKG-7 <pavel.k.group@gmail.com>
2022-12-14 17:01:48 +0300

    init

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
-import Head from "next/head";
-import Layout from "../components/Layout";
-import Image from "next/dist/client/image";
+import Head from 'next/head';
+import Layout from '../components/Layout';
+import Image from 'next/dist/client/image';
 import {
   ChevronDownIcon,
   PlusIcon,
   DotsVerticalIcon,
   PlusCircleIcon,
-} from "@heroicons/react/outline";
-import CardItem from "../components/CardItem";
-import BoardData from "../data/board-data.json";
-import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-import { useEffect, useState } from "react";
+} from '@heroicons/react/outline';
+import CardItem from '../components/CardItem';
+import BoardData from '../data/board-data.json';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { useEffect, useState } from 'react';
 
 function createGuidId() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
 }
@@ -24,6 +25,7 @@ export default function Home() {
   const [boardData, setBoardData] = useState(BoardData);
   const [showForm, setShowForm] = useState(false);
   const [selectedBoard, setSelectedBoard] = useState(0);
+  console.log(boardData);
 
   useEffect(() => {
     if (process.browser) {
@@ -31,7 +33,7 @@ export default function Home() {
     }
   }, []);
 
-  const onDragEnd = (re) => {
+  const onDragEnd = re => {
     if (!re.destination) return;
     let newBoardData = boardData;
     var dragItem =
@@ -48,23 +50,22 @@ export default function Home() {
     setBoardData(newBoardData);
   };
 
-  const onTextAreaKeyPress = (e) => {
-    if(e.keyCode === 13) //Enter
-    {
+  const onTextAreaKeyPress = e => {
+    if (e.keyCode === 13) {
+      //Enter
       const val = e.target.value;
-      if(val.length === 0) {
+      if (val.length === 0) {
         setShowForm(false);
-      }
-      else {
+      } else {
         const boardId = e.target.attributes['data-id'].value;
         const item = {
           id: createGuidId(),
           title: val,
           priority: 0,
-          chat:0,
+          chat: 0,
           attachment: 0,
-          assignees: []
-        }
+          assignees: [],
+        };
         let newBoardData = boardData;
         newBoardData[boardId].items.push(item);
         setBoardData(newBoardData);
@@ -72,55 +73,55 @@ export default function Home() {
         e.target.value = '';
       }
     }
-  }
+  };
 
   return (
     <Layout>
-      <div className="p-10 flex flex-col h-screen">
+      <div className='p-10 flex flex-col h-screen'>
         {/* Board header */}
-        <div className="flex flex-initial justify-between">
-          <div className="flex items-center">
-            <h4 className="text-4xl font-bold text-gray-600">Studio Board</h4>
+        <div className='flex flex-initial justify-between'>
+          <div className='flex items-center'>
+            <h4 className='text-4xl font-bold text-gray-600'>Studio Board</h4>
             <ChevronDownIcon
-              className="w-9 h-9 text-gray-500 rounded-full
-            p-1 bg-white ml-5 shadow-xl"
+              className='w-9 h-9 text-gray-500 rounded-full
+            p-1 bg-white ml-5 shadow-xl'
             />
           </div>
 
-          <ul className="flex space-x-3">
+          <ul className='flex space-x-3'>
             <li>
               <Image
-                src="https://randomuser.me/api/portraits/men/75.jpg"
-                width="36"
-                height="36"
-                objectFit="cover"
-                className=" rounded-full "
+                src='https://randomuser.me/api/portraits/men/75.jpg'
+                width='36'
+                height='36'
+                objectFit='cover'
+                className=' rounded-full '
               />
             </li>
             <li>
               <Image
-                src="https://randomuser.me/api/portraits/men/76.jpg"
-                width="36"
-                height="36"
-                objectFit="cover"
-                className=" rounded-full "
+                src='https://randomuser.me/api/portraits/men/76.jpg'
+                width='36'
+                height='36'
+                objectFit='cover'
+                className=' rounded-full '
               />
             </li>
             <li>
               <Image
-                src="https://randomuser.me/api/portraits/men/78.jpg"
-                width="36"
-                height="36"
-                objectFit="cover"
-                className=" rounded-full "
+                src='https://randomuser.me/api/portraits/men/78.jpg'
+                width='36'
+                height='36'
+                objectFit='cover'
+                className=' rounded-full '
               />
             </li>
             <li>
               <button
-                className="border border-dashed flex items-center w-9 h-9 border-gray-500 justify-center
-                rounded-full"
+                className='border border-dashed flex items-center w-9 h-9 border-gray-500 justify-center
+                rounded-full'
               >
-                <PlusIcon className="w-5 h-5 text-gray-500" />
+                <PlusIcon className='w-5 h-5 text-gray-500' />
               </button>
             </li>
           </ul>
@@ -129,7 +130,7 @@ export default function Home() {
         {/* Board columns */}
         {ready && (
           <DragDropContext onDragEnd={onDragEnd}>
-            <div className="grid grid-cols-4 gap-5 my-5">
+            <div className='grid grid-cols-4 gap-5 my-5'>
               {boardData.map((board, bIndex) => {
                 return (
                   <div key={board.name}>
@@ -142,21 +143,23 @@ export default function Home() {
                           <div
                             className={`bg-gray-100 rounded-md shadow-md
                             flex flex-col relative overflow-hidden
-                            ${snapshot.isDraggingOver && "bg-green-100"}`}
+                            ${snapshot.isDraggingOver && 'bg-green-100'}`}
                           >
                             <span
-                              className="w-full h-1 bg-gradient-to-r from-pink-700 to-red-200
-                          absolute inset-x-0 top-0"
+                              className='w-full h-1 bg-gradient-to-r from-pink-700 to-red-200
+                          absolute inset-x-0 top-0'
                             ></span>
-                            <h4 className=" p-3 flex justify-between items-center mb-2">
-                              <span className="text-2xl text-gray-600">
+                            <h4 className=' p-3 flex justify-between items-center mb-2'>
+                              <span className='text-2xl text-gray-600'>
                                 {board.name}
                               </span>
-                              <DotsVerticalIcon className="w-5 h-5 text-gray-500" />
+                              <DotsVerticalIcon className='w-5 h-5 text-gray-500' />
                             </h4>
 
-                            <div className="overflow-y-auto overflow-x-hidden h-auto"
-                            style={{maxHeight:'calc(100vh - 290px)'}}>
+                            <div
+                              className='overflow-y-auto overflow-x-hidden h-auto'
+                              style={{ maxHeight: 'calc(100vh - 290px)' }}
+                            >
                               {board.items.length > 0 &&
                                 board.items.map((item, iIndex) => {
                                   return (
@@ -164,31 +167,35 @@ export default function Home() {
                                       key={item.id}
                                       data={item}
                                       index={iIndex}
-                                      className="m-3"
+                                      className='m-3'
                                     />
                                   );
                                 })}
                               {provided.placeholder}
                             </div>
-                            
-                            {
-                              showForm && selectedBoard === bIndex ? (
-                                <div className="p-3">
-                                  <textarea className="border-gray-300 rounded focus:ring-purple-400 w-full" 
-                                  rows={3} placeholder="Task info" 
+
+                            {showForm && selectedBoard === bIndex ? (
+                              <div className='p-3'>
+                                <textarea
+                                  className='border-gray-300 rounded focus:ring-purple-400 w-full'
+                                  rows={3}
+                                  placeholder='Task info'
                                   data-id={bIndex}
-                                  onKeyDown={(e) => onTextAreaKeyPress(e)}/>
-                                </div>
-                              ): (
-                                <button
-                                  className="flex justify-center items-center my-3 space-x-2 text-lg"
-                                  onClick={() => {setSelectedBoard(bIndex); setShowForm(true);}}
-                                >
-                                  <span>Add task</span>
-                                  <PlusCircleIcon className="w-5 h-5 text-gray-500" />
-                                </button>
-                              )
-                            }
+                                  onKeyDown={e => onTextAreaKeyPress(e)}
+                                />
+                              </div>
+                            ) : (
+                              <button
+                                className='flex justify-center items-center my-3 space-x-2 text-lg'
+                                onClick={() => {
+                                  setSelectedBoard(bIndex);
+                                  setShowForm(true);
+                                }}
+                              >
+                                <span>Add task</span>
+                                <PlusCircleIcon className='w-5 h-5 text-gray-500' />
+                              </button>
+                            )}
                           </div>
                         </div>
                       )}
